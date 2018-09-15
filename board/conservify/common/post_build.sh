b9563a6fc4510b0c7883c3c268ea524c0759514a
Jacob Lewallen <jlewallen@gmail.com>
2018-09-15 13:37:37 -0700

    Pregenerate host keys and include user's public key for auth.

--- board/conservify/common/post_build.sh
+++ board/conservify/common/post_build.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# Create dropbear host key.
+if [ ! -d ${TARGET_DIR}/etc/dropbear ]; then
+    rm ${TARGET_DIR}/etc/dropbear
+    mkdir -p ${TARGET_DIR}/etc/dropbear
+fi
+
+if [ ! -f ${TARGET_DIR}/etc/dropbear/dropbear_rsa_host_key ]; then
+    dropbearkey -t rsa -f ${TARGET_DIR}/etc/dropbear/dropbear_rsa_host_key
+fi
+
+# Allow the user creating the image to login over SSH.
+if [ -f ~/.ssh/id_rsa.pub ]; then
+    if [ -f ${TARGET_DIR}/root/.ssh/authorized_keys ]; then
+        rm ${TARGET_DIR}/root/.ssh/authorized_keys
+    fi
+
+    cp ~/.ssh/id_rsa.pub ${TARGET_DIR}/root/.ssh/authorized_keys
+fi
