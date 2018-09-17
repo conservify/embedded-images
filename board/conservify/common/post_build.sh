4b09759cd214199ec69d1b12c2f5881cbd33ef4e
Jacob Lewallen <jlewallen@gmail.com>
2018-09-17 15:58:42 -0700

    Copy wifi configuration from private area.

--- board/conservify/common/post_build.sh
+++ board/conservify/common/post_build.sh
@@ -40,20 +40,24 @@ if [ -d $PRIVATE ]; then
     HOSTNAME=$(head -n 1 ${TARGET_DIR}/etc/hostname)
     echo "Using HOSTNAME=${HOSTNAME}"
 
+    # Keys, for tunnels abd backups, etc...
     mkdir -p ${TARGET_PRIVATE}
     chmod 700 ${TARGET_PRIVATE}
     cp ${PRIVATE}/*.id_rsa* ${TARGET_PRIVATE}
     chmod 600 ${TARGET_PRIVATE}/*.id_rsa
     chmod 600 ${TARGET_PRIVATE}/*.id_rsa.pub
 
+    # Setup default user key(s).
     if [ -f ${PRIVATE}/user.id_rsa.dropbear ]; then
-        SSH_DIR=${TARGET_DIR}/root/.ssh
-        mkdir -p ${SSH_DIR}
-        cp ${PRIVATE}/user.id_rsa ${SSH_DIR}/id_rsa.openssh
-        cp ${PRIVATE}/user.id_rsa.dropbear ${SSH_DIR}/id_rsa
-        cp ${PRIVATE}/user.id_rsa.pub ${SSH_DIR}/id_rsa.pub
-        chmod 700 ${SSH_DIR}
-        chmod 600 ${SSH_DIR}/id_rsa*
+        for user in root; do
+            SSH_DIR=${TARGET_DIR}/${user}/.ssh
+            mkdir -p ${SSH_DIR}
+            cp ${PRIVATE}/user.id_rsa ${SSH_DIR}/id_rsa.openssh
+            cp ${PRIVATE}/user.id_rsa.dropbear ${SSH_DIR}/id_rsa
+            cp ${PRIVATE}/user.id_rsa.pub ${SSH_DIR}/id_rsa.pub
+            chmod 700 ${SSH_DIR}
+            chmod 600 ${SSH_DIR}/id_rsa*
+        done
     fi
 
     # Copy per-machine configuration.
@@ -61,6 +65,13 @@ if [ -d $PRIVATE ]; then
         cp -ar ${PRIVATE}/${HOSTNAME}.env ${TARGET_PRIVATE}/private.env
     else
         echo "No per-machine configuration found!"
+    fi
+
+    # Copy wifi configuration.
+    if [ -f ${PRIVATE}/wifi/wpa_supplicant.conf ]; then
+        cp -ar ${PRIVATE}/wifi/wpa_supplicant.conf ${TARGET_PRIVATE}
+    else
+        echo "No wifi configuration found!"
     fi
 
     # Copy wireguard setup from secure/private area.
