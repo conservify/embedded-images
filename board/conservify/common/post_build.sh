d5e5948d128dbb93b01f88e74388ba2f89e00d19
Jacob Lewallen <jlewallen@gmail.com>
2018-09-16 17:43:33 -0700

    Copy user keys from private area.

--- board/conservify/common/post_build.sh
+++ board/conservify/common/post_build.sh
@@ -26,29 +26,43 @@ fi
 # ----------------------------------------------------------
 # Create and copy private area
 PRIVATE=${BR2_EXTERNAL_CONSERVIFY_PATH}/private
-TARGET_PRIVATE=${TARGET_DIR}/etc/private
-HOSTNAME=$(head -n 1 ${TARGET_DIR}/etc/hostname)
-echo "Using HOSTNAME=${HOSTNAME}"
+if [ -d $PRIVATE ]; then
+    TARGET_PRIVATE=${TARGET_DIR}/etc/private
+    HOSTNAME=$(head -n 1 ${TARGET_DIR}/etc/hostname)
+    echo "Using HOSTNAME=${HOSTNAME}"
 
-mkdir -p ${TARGET_PRIVATE}
-chmod 700 ${TARGET_PRIVATE}
-cp ${PRIVATE}/*.id_rsa* ${TARGET_PRIVATE}
-chmod 600 ${TARGET_PRIVATE}/*.id_rsa
-chmod 600 ${TARGET_PRIVATE}/*.id_rsa.pub
+    mkdir -p ${TARGET_PRIVATE}
+    chmod 700 ${TARGET_PRIVATE}
+    cp ${PRIVATE}/*.id_rsa* ${TARGET_PRIVATE}
+    chmod 600 ${TARGET_PRIVATE}/*.id_rsa
+    chmod 600 ${TARGET_PRIVATE}/*.id_rsa.pub
 
-# Copy per-machine configuration.
-if [ -f ${PRIVATE}/${HOSTNAME}.env ]; then
-    cp -ar ${PRIVATE}/${HOSTNAME}.env ${TARGET_PRIVATE}/private.env
+    if [ -f ${PRIVATE}/user.id_rsa.dropbear ]; then
+        SSH_DIR=${TARGET_DIR}/root/.ssh
+        mkdir -p ${SSH_DIR}
+        cp ${PRIVATE}/user.id_rsa ${SSH_DIR}/id_rsa.openssh
+        cp ${PRIVATE}/user.id_rsa.dropbear ${SSH_DIR}/id_rsa
+        cp ${PRIVATE}/user.id_rsa.pub ${SSH_DIR}/id_rsa.pub
+        chmod 700 ${SSH_DIR}
+        chmod 600 ${SSH_DIR}/id_rsa*
+    fi
+
+    # Copy per-machine configuration.
+    if [ -f ${PRIVATE}/${HOSTNAME}.env ]; then
+        cp -ar ${PRIVATE}/${HOSTNAME}.env ${TARGET_PRIVATE}/private.env
+    else
+        echo "No per-machine configuration found!"
+    fi
+
+    # Copy wireguard setup from secure/private area.
+    if [ -d ${PRIVATE}/wg/${HOSTNAME} ]; then
+        mkdir -p ${TARGET_PRIVATE}/wg
+        cp -ar ${PRIVATE}/wg/${HOSTNAME}/* ${TARGET_PRIVATE}/wg
+        chmod 700 ${TARGET_PRIVATE}/wg
+        chmod 600 ${TARGET_PRIVATE}/wg/*
+    else
+        echo "No wireguard configuration found!"
+    fi
 else
-    echo "No per-machine configuration found!"
-fi
-
-# Copy wireguard setup from secure/private area.
-if [ -d ${PRIVATE}/wg/${HOSTNAME} ]; then
-    mkdir -p ${TARGET_PRIVATE}/wg
-    cp -ar ${PRIVATE}/wg/${HOSTNAME}/* ${TARGET_PRIVATE}/wg
-    chmod 700 ${TARGET_PRIVATE}/wg
-    chmod 600 ${TARGET_PRIVATE}/wg/*
-else
-    echo "No wireguard configuration found!"
+    echo "No local private area."
 fi
