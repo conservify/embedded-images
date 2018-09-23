db3644bf10681280f290010cc1f08a7dfe2a17c2
Jacob Lewallen <jlewallen@gmail.com>
2018-09-23 11:21:33 -0700

    Create missing directories.

--- package/conservify-network/conservify-network.mk
+++ package/conservify-network/conservify-network.mk
@@ -8,10 +8,12 @@ define CONSERVIFY_NETWORK_BUILD_CMDS
 endef
 
 define CONSERVIFY_NETWORK_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(CONSERVIFY_NETWORK_PKGDIR)/S41*
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/local/bin
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/local/bin $(CONSERVIFY_NETWORK_PKGDIR)/network-finder
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/local/bin $(CONSERVIFY_NETWORK_PKGDIR)/network-watcher
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/etc/network/online
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/network/online $(CONSERVIFY_NETWORK_PKGDIR)/S01wireguard
-	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(CONSERVIFY_NETWORK_PKGDIR)/S41*
 endef
 
 $(eval $(generic-package))
--- package/conservify-tunnels/conservify-tunnels.mk
+++ package/conservify-tunnels/conservify-tunnels.mk
@@ -8,6 +8,7 @@ define CONSERVIFY_TUNNELS_BUILD_CMDS
 endef
 
 define CONSERVIFY_TUNNELS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/local/bin
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/local/bin $(CONSERVIFY_TUNNELS_PKGDIR)/ssh-tunnel
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/usr/local/bin $(CONSERVIFY_TUNNELS_PKGDIR)/rsyslog-tunnel
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(CONSERVIFY_TUNNELS_PKGDIR)/S??*
