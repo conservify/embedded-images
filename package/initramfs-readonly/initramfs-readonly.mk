e6b6c2219e2a14a3c14bdf2ec2a7bf6b2970e177
Jacob Lewallen <jlewallen@gmail.com>
2018-09-23 11:45:22 -0700

    Include wifi firmware in readonly initramfs.

--- package/initramfs-readonly/initramfs-readonly.mk
+++ package/initramfs-readonly/initramfs-readonly.mk
@@ -6,18 +6,23 @@
 
 INITRAMFS_READONLY_VERSION = master
 INITRAMFS_READONLY_SITE = $(call github,raspberrypi,target_fs,$(INITRAMFS_READONLY_VERSION))
+INITRAMFS_READONLY_DEPENDENCIES = rpi-firmware rpi-wifi-firmware
 
 define INITRAMFS_READONLY_BUILD_CMDS
 	rm -f $(@D)/init
+	rm -f $(@D)/etc/mdev.conf
+
 	cp $(INITRAMFS_READONLY_PKGDIR)/init $(@D)
 	chmod 755 $(@D)/init
-	rm -f $(@D)/etc/mdev.conf
-	mkdir -p $(@D)/boot
-	mkdir -p $(@D)/mnt/root-ro
-	mkdir -p $(@D)/mnt/root-rw
-	mkdir -p $(@D)/rootfs
-	mkdir -p $(@D)/rootfs/root-ro
-	mkdir -p $(@D)/rootfs/root-rw
+
+  $(INSTALL) -m 0755 -d $(@D)/boot
+  $(INSTALL) -m 0755 -d $(@D)/mnt/root-ro
+  $(INSTALL) -m 0755 -d $(@D)/mnt/root-rw
+  $(INSTALL) -m 0755 -d $(@D)/rootfs/root-ro
+  $(INSTALL) -m 0755 -d $(@D)/rootfs/root-rw
+  $(INSTALL) -m 0755 -d $(@D)/lib/firmware/brcm
+  $(INSTALL) -m 0644 -t $(@D)/lib/firmware/brcm/ $(TARGET_DIR)/lib/firmware/brcm/*
+
 	(cd $(@D) && find . -name .git -a -type d -prune -o -name "initramfs.*" -type f -prune -o -print | cpio -o -H newc > $(@D)/initramfs.cpio)
 	gzip -c $(@D)/initramfs.cpio > $(@D)/initramfs.cpio.gz
 endef
