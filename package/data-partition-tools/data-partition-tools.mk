4e780f557c103b0bc0da3f53dd83b3dfd51c2a76
Jacob Lewallen <jlewallen@gmail.com>
2018-09-24 16:48:42 -0700

    Startup script to provision the data partition.

--- package/data-partition-tools/data-partition-tools.mk
+++ package/data-partition-tools/data-partition-tools.mk
@@ -9,6 +9,7 @@ endef
 
 define DATA_PARTITION_TOOLS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -t $(TARGET_DIR)/bin $(DATA_PARTITION_TOOLS_PKGDIR)/create-data-partition.sh
+	$(INSTALL) -m 0755 -t $(TARGET_DIR)/etc/init.d $(DATA_PARTITION_TOOLS_PKGDIR)/S??data-partition
 endef
 
 $(eval $(generic-package))
